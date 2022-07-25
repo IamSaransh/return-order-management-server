587d5b781eb6be6a8c7e2525440242b7521004e1
Saransh <saranshvashistha@gmail.com>
2022-07-25 16:50:03 +0530

    "Feat: PackagingAndDelivery service module added"

---
+++ packaging-and-delivery/src/main/java/me/saransh13/PackagingAndDeliveryApplication.java
@@ -0,0 +1,13 @@
+package me.saransh13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PackagingAndDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PackagingAndDeliveryApplication.class, args);
+	}
+
+}
