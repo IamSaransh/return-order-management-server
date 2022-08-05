ef7d98412c0aa4a627280e371d149d72af74aaab
Saransh <saranshvashistha@gmail.com>
2022-08-05 14:14:07 +0530

    "feat: add sonar scan"

--- authorization-server/buildscripts/build.sh
+++ authorization-server/buildscripts/build.sh
@@ -2,6 +2,8 @@
 echo "navigating to your directory"
 cd "${BASH_SOURCE%/*}" || exit
 cd ..
+export SONAR_TOKEN=715fc0eac466bc9b5e551b4fc695894e02543492
+echo "sonar token is : $SONAR_TOKEN"
 
 echo 'Running jacoco:prepare-agent...'
 mvn clean jacoco:prepare-agent install
@@ -11,4 +13,4 @@ echo 'Running sonar:sonar...'
 mvn sonar:sonar \
   -Dsonar.projectKey=authorization \
   -Dsonar.host.url=https://sonarcloud.io \
-  -Dsonar.login=dc7fa119cf886f7d7c604216663bbf1cfc067f8a
+  -Dsonar.organization=iamsaransh
